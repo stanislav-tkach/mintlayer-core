928242d4c559f1cbf15075b37e92e43443f814e7
Amin Razavi <amin.razavi@mintlayer.org>
2021-12-02 19:15:43 +0400

    solve dynamic clone dependencies

--- rpc/api/src/traits.rs
+++ rpc/api/src/traits.rs
@@ -1,89 +1,91 @@
-
+use dyn_clone::DynClone;
 /// Something that can spawn tasks (blocking and non-blocking) with an assigned name
 /// and optional group.
-#[dyn_clonable::clonable]
-pub trait SpawnNamed: Clone + Send + Sync {
-	/// Spawn the given blocking future.
-	///
-	/// The given `group` and `name` is used to identify the future in tracing.
-	fn spawn_blocking(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	);
-	/// Spawn the given non-blocking future.
-	///
-	/// The given `group` and `name` is used to identify the future in tracing.
-	fn spawn(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	);
+pub trait SpawnNamed: DynClone + Send + Sync {
+    /// Spawn the given blocking future.
+    ///
+    /// The given `group` and `name` is used to identify the future in tracing.
+    fn spawn_blocking(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    );
+    /// Spawn the given non-blocking future.
+    ///
+    /// The given `group` and `name` is used to identify the future in tracing.
+    fn spawn(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    );
 }
 
+dyn_clone::clone_trait_object!(SpawnNamed);
+
 impl SpawnNamed for Box<dyn SpawnNamed> {
-	fn spawn_blocking(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	) {
-		(**self).spawn_blocking(name, group, future)
-	}
-	fn spawn(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	) {
-		(**self).spawn(name, group, future)
-	}
+    fn spawn_blocking(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    ) {
+        (**self).spawn_blocking(name, group, future)
+    }
+    fn spawn(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    ) {
+        (**self).spawn(name, group, future)
+    }
 }
 
 /// Something that can spawn essential tasks (blocking and non-blocking) with an assigned name
 /// and optional group.
 ///
 /// Essential tasks are special tasks that should take down the node when they end.
-#[dyn_clonable::clonable]
-pub trait SpawnEssentialNamed: Clone + Send + Sync {
-	/// Spawn the given blocking future.
-	///
-	/// The given `group` and `name` is used to identify the future in tracing.
-	fn spawn_essential_blocking(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	);
-	/// Spawn the given non-blocking future.
-	///
-	/// The given `group` and `name` is used to identify the future in tracing.
-	fn spawn_essential(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	);
+pub trait SpawnEssentialNamed: DynClone + Send + Sync {
+    /// Spawn the given blocking future.
+    ///
+    /// The given `group` and `name` is used to identify the future in tracing.
+    fn spawn_essential_blocking(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    );
+    /// Spawn the given non-blocking future.
+    ///
+    /// The given `group` and `name` is used to identify the future in tracing.
+    fn spawn_essential(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    );
 }
 
-impl SpawnEssentialNamed for Box<dyn SpawnEssentialNamed> {
-	fn spawn_essential_blocking(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	) {
-		(**self).spawn_essential_blocking(name, group, future)
-	}
+dyn_clone::clone_trait_object!(SpawnEssentialNamed);
 
-	fn spawn_essential(
-		&self,
-		name: &'static str,
-		group: Option<&'static str>,
-		future: futures::future::BoxFuture<'static, ()>,
-	) {
-		(**self).spawn_essential(name, group, future)
-	}
+impl SpawnEssentialNamed for Box<dyn SpawnEssentialNamed> {
+    fn spawn_essential_blocking(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    ) {
+        (**self).spawn_essential_blocking(name, group, future)
+    }
+
+    fn spawn_essential(
+        &self,
+        name: &'static str,
+        group: Option<&'static str>,
+        future: futures::future::BoxFuture<'static, ()>,
+    ) {
+        (**self).spawn_essential(name, group, future)
+    }
 }
