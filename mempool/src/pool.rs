2aafaea8eb009028fc18e2d837addaa2ca902232
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-02-15 15:58:06 +0700

    replacement fee higher than original in unit test

--- mempool/src/pool.rs
+++ mempool/src/pool.rs
@@ -1099,10 +1099,11 @@ mod tests {
 
         let original_fee = Amount::from_atoms(10);
         let dummy_output = TxOutput::new(original_fee, Destination::PublicKey);
-        let replaced_tx = Transaction::new(
+        let replaced_tx = tx_spend_several_inputs(
+            &mempool,
+            &[input_with_irreplaceable_parent.clone(), input_with_replaceable_parent],
+            original_fee,
             flags_irreplaceable,
-            vec![input_with_irreplaceable_parent.clone(), input_with_replaceable_parent],
-            vec![dummy_output.clone()],
             locktime,
         )?;
 
