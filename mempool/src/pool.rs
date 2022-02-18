409c8225efc2d0f1732fb5f241b8fafd0de0e16f
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-02-18 13:45:12 +0700

    Fixed tx_spend_inputs

--- mempool/src/pool.rs
+++ mempool/src/pool.rs
@@ -1143,6 +1143,7 @@ mod tests {
         flags: u32,
         locktime: u32,
     ) -> anyhow::Result<Transaction> {
+        let fee = fee.into().map_or(Amount::from_atoms(0), std::convert::identity);
         let input_value = inputs
             .iter()
             .map(|input| mempool.get_input_value(input))
@@ -1151,23 +1152,17 @@ mod tests {
             .sum::<Option<_>>()
             .expect("tx_spend_input: overflow");
 
-        let output_value = if let Some(fee) = fee.into() {
-            (fee <= input_value)
-                .then(|| (input_value - fee).expect("tx_spend_input: subtraction error"))
-                .ok_or(anyhow::anyhow!("Not enough funds"))?
-        } else {
-            (input_value / 2).expect("tx_spend_input: division error")
-        };
+        let available_for_spending = (input_value - fee).expect("underflow");
+        let spent = (available_for_spending / 2).expect("division error");
+
+        let change = (available_for_spending - spent).expect("underflow");
 
         Transaction::new(
             flags,
             inputs.to_owned(),
             vec![
-                TxOutput::new(output_value, Destination::PublicKey),
-                TxOutput::new(
-                    (input_value - output_value).expect("underflow"),
-                    Destination::PublicKey,
-                ),
+                TxOutput::new(spent, Destination::PublicKey),
+                TxOutput::new(change, Destination::PublicKey),
             ],
             locktime,
         )
