6a3d87bda6f46b58f203fb0f03572b4dc011dd86
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-03-07 17:56:32 +0700

    Add pow function to amount

--- common/src/primitives/amount.rs
+++ common/src/primitives/amount.rs
@@ -108,6 +108,13 @@ impl Amount {
     pub fn random(range: std::ops::RangeInclusive<Amount>) -> Amount {
         rand::thread_rng().gen_range(range.start().val..=range.end().val).into()
     }
+
+    pub fn pow(self, exponent: usize) -> Option<Self> {
+        (0..exponent).into_iter().try_fold(Amount::from(1), |mut partial_result, _| {
+            partial_result = (partial_result * self)?;
+            Some(partial_result)
+        })
+    }
 }
 
 impl From<u128> for Amount {
@@ -724,5 +731,11 @@ mod tests {
         assert_eq!(Amount { val: 1234567890100 }.into_fixedpoint_str(1), "123456789010");
         assert_eq!(Amount { val: 12345678901200 }.into_fixedpoint_str(1), "1234567890120");
         assert_eq!(Amount { val: 123456789012300 }.into_fixedpoint_str(1), "12345678901230");
+    }
+
+    #[test]
+    fn pow() {
+        let x = Amount { val: 2 };
+        assert_eq!(x.pow(4), Some(Amount { val: 16 }));
     }
 }
