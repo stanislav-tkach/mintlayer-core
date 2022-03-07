37fbad55e481baac435613010d566cb21b7c1a6a
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-03-07 10:21:14 +0700

    Added utils crate with newtype macro

---
+++ utils/src/newtype.rs
@@ -0,0 +1,31 @@
+#[macro_export]
+macro_rules! newtype {
+    ($vis:vis $name:ident($wrapped:ty)) => {
+        $vis struct $name($wrapped);
+
+        impl From<$name> for $wrapped {
+            fn from(newtype_instance: $name) -> Self {
+                newtype_instance.0
+            }
+        }
+
+        impl From<$wrapped> for $name {
+            fn from(inner: $wrapped) -> Self {
+                Self(inner)
+            }
+        }
+
+        impl std::ops::Deref for $name {
+            type Target = $wrapped;
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl std::ops::DerefMut for $name {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.0
+            }
+        }
+    };
+}
