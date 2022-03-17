b645f428418c5c8bd99f769688dfabeb45c7ace5
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-03-17 13:57:59 +0700

    rename random_unspent_outpoint -> get_unspent_outpoint

--- mempool/src/pool.rs
+++ mempool/src/pool.rs
@@ -1305,7 +1305,7 @@ mod tests {
         }
 
         fn generate_input(&self) -> anyhow::Result<(TxInput, Amount)> {
-            let ValuedOutPoint { outpoint, value } = self.random_unspent_outpoint()?;
+            let ValuedOutPoint { outpoint, value } = self.get_unspent_outpoint()?;
             Ok((
                 TxInput::new(
                     outpoint.get_tx_id(),
@@ -1316,7 +1316,7 @@ mod tests {
             ))
         }
 
-        fn random_unspent_outpoint(&self) -> anyhow::Result<ValuedOutPoint> {
+        fn get_unspent_outpoint(&self) -> anyhow::Result<ValuedOutPoint> {
             let num_outpoints = self.coin_pool.len();
             (num_outpoints > 0)
                 .then(|| {
