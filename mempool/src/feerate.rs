ae06014a0b2b37e7e88da64916fa04155e5eefb7
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-04-11 19:14:47 +0700

    Fix FeeRate calculations to consistently use tokens/kb and update rolling_fee test accordingly

--- mempool/src/feerate.rs
+++ mempool/src/feerate.rs
@@ -4,30 +4,42 @@ use crate::error::TxValidationError;
 
 lazy_static::lazy_static! {
     pub(crate) static ref INCREMENTAL_RELAY_FEE_RATE: FeeRate = FeeRate::new(1000);
+    pub(crate) static ref INCREMENTAL_RELAY_THRESHOLD: FeeRate = FeeRate::new(500);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub(crate) struct FeeRate {
-    tokens_per_byte: Amount,
+    tokens_per_kb: Amount,
 }
 
 impl FeeRate {
-    pub(crate) fn new(tokens_per_byte: impl Into<Amount>) -> Self {
-        let tokens_per_byte: Amount = tokens_per_byte.into();
-        Self { tokens_per_byte }
+    pub(crate) fn new(tokens_per_kb: impl Into<Amount>) -> Self {
+        let tokens_per_kb: Amount = tokens_per_kb.into();
+        Self { tokens_per_kb }
     }
 
     pub(crate) fn of_tx(fee: Amount, tx_size: usize) -> Result<Self, TxValidationError> {
         let tokens_per_byte = Self::div_up(fee, tx_size)?;
-        Ok(Self { tokens_per_byte })
+        Ok(Self {
+            tokens_per_kb: (tokens_per_byte * Amount::from(1000)).unwrap(),
+        })
     }
 
     pub(crate) fn compute_fee(&self, size: usize) -> Result<Amount, TxValidationError> {
-        (self.tokens_per_byte * Amount::from(size as u128)).ok_or(TxValidationError::FeeRateError)
+        let res = ((self.tokens_per_kb * Amount::from(u128::try_from(size).unwrap())).unwrap()
+            / Amount::from(1000))
+        .ok_or(TxValidationError::FeeRateError);
+        logging::log::debug!(
+            "computed fee {:?}, for a tx of size {}, when the fee rate in tokens_per_kb is {:?}",
+            res,
+            size,
+            self.tokens_per_kb
+        );
+        res
     }
 
-    pub(crate) fn tokens_per_byte(&self) -> Amount {
-        self.tokens_per_byte
+    pub(crate) fn tokens_per_kb(&self) -> Amount {
+        self.tokens_per_kb
     }
 
     fn div_up(fee: Amount, tx_size: usize) -> Result<Amount, TxValidationError> {
@@ -42,15 +54,19 @@ impl FeeRate {
 impl std::ops::Add for FeeRate {
     type Output = Option<FeeRate>;
     fn add(self, other: Self) -> Self::Output {
-        let tokens_per_byte = self.tokens_per_byte + other.tokens_per_byte;
-        tokens_per_byte.map(|tokens_per_byte| FeeRate { tokens_per_byte })
+        let tokens_per_byte = self.tokens_per_kb + other.tokens_per_kb;
+        tokens_per_byte.map(|tokens_per_byte| FeeRate {
+            tokens_per_kb: tokens_per_byte,
+        })
     }
 }
 
 impl std::ops::Div for FeeRate {
     type Output = Option<FeeRate>;
     fn div(self, other: Self) -> Self::Output {
-        let tokens_per_byte = self.tokens_per_byte / other.tokens_per_byte;
-        tokens_per_byte.map(|tokens_per_byte| FeeRate { tokens_per_byte })
+        let tokens_per_byte = self.tokens_per_kb / other.tokens_per_kb;
+        tokens_per_byte.map(|tokens_per_byte| FeeRate {
+            tokens_per_kb: tokens_per_byte,
+        })
     }
 }
