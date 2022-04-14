cf51e18b06288bf6591e0add6138f165ba9767c5
Roy Ben Abraham <roy.benabraham@rbblab.com>
2022-04-14 14:41:25 +0700

    Elegant calls on client side

--- jsonrpc/client/src/main.rs
+++ jsonrpc/client/src/main.rs
@@ -9,20 +9,29 @@ use jsonrpc_core_client::RawClient;
 use jsonrpc_derive::rpc;
 use serde_json::value::Number;
 
+/// Rpc trait
+#[rpc]
+pub trait Rpc {
+    /// Returns a protocol version
+    #[rpc(name = "protocolVersion")]
+    fn protocol_version(&self) -> Result<String>;
+
+    /// Adds two numbers and returns a result
+    #[rpc(name = "add", alias("callAsyncMetaAlias"))]
+    fn add(&self, a: u64, b: u64) -> Result<u64>;
+
+    /// Performs asynchronous operation
+    #[rpc(name = "callAsync")]
+    fn call(&self, a: u64) -> BoxFuture<Result<String>>;
+}
+
 #[tokio::main]
 async fn main() {
-    let client = http::connect::<RawClient>("http://127.0.0.1:3030")
+    let client = http::connect::<RpcClient>("http://127.0.0.1:3030")
         .await
         .expect("create client");
-    let res = client
-        .call_method(
-            "add",
-            Params::Array(vec![
-                Value::Number(Number::from(120)),
-                Value::Number(Number::from(360)),
-            ]),
-        )
-        .await;
+
+    let res = client.add(5, 6).await;
 
     println!("result: {:?}", res);
 }
