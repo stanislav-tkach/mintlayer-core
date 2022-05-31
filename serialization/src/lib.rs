797725442eecd46e919228dfdaa2994b094382a9
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2022-05-31 23:00:42 +0100

    serialization: Size-dependent encoding support

--- serialization/src/lib.rs
+++ serialization/src/lib.rs
@@ -1,7 +1,12 @@
 //! Blockchain data encoding and decoding tools
 
+pub mod size_dependent;
+
 // Re-export traits
 pub use parity_scale_codec::{Codec, Decode, DecodeAll, Encode, Input, Output};
 
 // Re-export types
 pub use parity_scale_codec::Error;
+
+// SCALE types and traits used by this crate only
+use parity_scale_codec::{Compact, EncodeAsRef};
